8cc7fd4ed7705c9928cd9499158de417e2cbb67d
JustFantick <kotelevskijdanil0@gmail.com>
2024-05-12 12:52:58 +0300

    feat: init repo

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Nunito_Sans } from 'next/font/google';
+import Header from '@/components/header/Header';
+import './globals.scss';
+
+const font = Nunito_Sans({
+	subsets: ['latin'],
+	weight: ['400', '500', '600', '700'],
+});
+
+export const metadata: Metadata = {
+	title: 'Menu mastermind',
+	description: 'Recipes for everyone',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang='en'>
+			<body className={font.className}>
+				<Header></Header>
+				<div className='wrapper'>{children}</div>
+				<footer></footer>
+			</body>
+		</html>
+	);
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './header.module.scss';
+
+const Header = () => {
+	return <header>Header</header>;
+};
+
+export default Header;

d6f25f33730843cef76dabd7ae537df550852886
JustFantick <kotelevskijdanil0gmail.com>
2024-05-16 09:53:26 +0300

    feat(app): set home page bgi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Searchbar from '@/components/searchbar/Searchbar';
+import Image from 'next/image';
 import styles from './page.module.scss';
 
 export default function Home() {
 	return (
 		<main className={styles.home}>
-			{/* <Image/> */}
+			<section className={styles.home__section}>
+				<Image src={'/home-bg_2.png'} alt='home-bg' layout='fill' objectFit='cover' className={styles.sectionBackground} />
+				<div className={styles.overlay}></div>
 
-			<Searchbar />
+				<Searchbar />
+			</section>
 		</main>
 	);
 }

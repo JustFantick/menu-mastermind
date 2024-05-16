5c727ab233757d982289f96321aad0b3f172e11d
JustFantick <kotelevskijdanil0gmail.com>
2024-05-16 09:34:59 +0300

    feat(components): complete Searchbar

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+'use client';
 
-const Searchbar = () => {
+import Image from 'next/image';
+import React, { useState } from 'react';
+import styles from './searchbar.module.scss';
+
+interface SearchbarProps {
+	defaultValue?: string;
+	placeholder?: string;
+	onSubmit?: (value: String) => void;
+}
+
+const Searchbar = ({ defaultValue, placeholder, onSubmit }: SearchbarProps) => {
+	const [inputValue, setInputValue] = useState(defaultValue || '');
+
+	function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+		setInputValue(e.target.value);
+	}
+
+	function onKeyPressHandler(e: React.KeyboardEvent<HTMLInputElement>) {
+		if (!onSubmit) return;
+
+		if (e.key === 'Enter') {
+			onSubmit(inputValue);
+		}
+	}
+
 	return (
-		<label>
-			<input type='text' name='search' placeholder='Поиск' />
+		<label className={styles.searchbar}>
+			<input
+				type='text'
+				name='search'
+				placeholder={placeholder ? placeholder : 'Search'}
+				defaultValue={defaultValue && defaultValue}
+				className={styles.searchbar__input}
+				onChange={e => onChangeHandler(e)}
+				onKeyDown={onKeyPressHandler}
+			/>
+
+			<div className={styles.searchbar__imgSection}>
+				<Image src={'/faq-icon.svg'} height={35} width={35} alt='faq-icon' />
+			</div>
 		</label>
 	);
 };

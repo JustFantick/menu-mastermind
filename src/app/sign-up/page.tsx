421e8dac561db1cadfd0b2b1630b16bc46582d13
JustFantick <kotelevskijdanil0gmail.com>
2024-05-23 11:30:35 +0300

    feat: init prisma client & test db access

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import s from './page.module.scss';
 import SignUpForm from '@/components/form/SignUpForm';
+import prisma from '@/lib/db';
 
-const SignUpPage = () => {
+const SignUpPage = async () => {
+	const users = await prisma.users.findMany();
+	console.log(users);
+
 	return (
 		<div className={s.signUp}>
 			<SignUpForm />

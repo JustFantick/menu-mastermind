9f28ec5299ddc70f19f82e7ad90973cb827386fa
JustFantick <kotelevskijdanil0gmail.com>
2024-05-24 11:01:37 +0300

    refactor: remove unnecessary log

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -4,9 +4,6 @@ import SignUpForm from '@/components/form/SignUpForm';
 import prisma from '@/lib/db';
 
 const SignUpPage = async () => {
-	const users = await prisma.users.findMany();
-	console.log(users);
-
 	return (
 		<div className={s.signUp}>
 			<SignUpForm />

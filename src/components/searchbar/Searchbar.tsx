d2b1629d56cf43de1c71beb85ab2298f4bca0a85
JustFantick <kotelevskijdanil0gmail.com>
2024-05-25 11:11:23 +0300

    feat: update Searchbar logic & styles

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,46 +1,47 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './searchbar.module.scss';
 
 interface SearchbarProps {
 	defaultValue?: string;
 	placeholder?: string;
-	onSubmit?: (value: String) => void;
+	onSubmit: (value: string) => void;
 }
 
 const Searchbar = ({ defaultValue, placeholder, onSubmit }: SearchbarProps) => {
-	const [inputValue, setInputValue] = useState(defaultValue || '');
+	const [inputValue, setInputValue] = useState<string>(defaultValue || '');
+
+	useEffect(() => {
+		setInputValue(defaultValue || '');
+	}, [defaultValue]);
 
 	function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
 		setInputValue(e.target.value);
 	}
 
-	function onKeyPressHandler(e: React.KeyboardEvent<HTMLInputElement>) {
-		if (!onSubmit) return;
-
-		if (e.key === 'Enter') {
-			onSubmit(inputValue);
-		}
-	}
-
 	return (
-		<label className={styles.searchbar}>
+		<form
+			className={styles.searchbar}
+			onSubmit={e => {
+				e.preventDefault();
+				if (inputValue !== '') onSubmit(inputValue);
+			}}
+		>
 			<input
+				className={styles.searchbar__input}
 				type='text'
 				name='search'
 				placeholder={placeholder ? placeholder : 'Search'}
-				defaultValue={defaultValue && defaultValue}
-				className={styles.searchbar__input}
+				value={inputValue}
 				onChange={e => onChangeHandler(e)}
-				onKeyDown={onKeyPressHandler}
 			/>
 
 			<div className={styles.searchbar__imgSection}>
 				<Image src={'/faq-icon.svg'} height={35} width={35} alt='faq-icon' />
 			</div>
-		</label>
+		</form>
 	);
 };
 

0b9da21ebaf19fb6a63a53cdb1cff6a3cd056efe
JustFantick <kotelevskijdanil0gmail.com>
2024-05-25 19:40:56 +0300

    feat: set actual recipe info

--- src/app/recipe/[recipeId]/page.tsx
+++ src/app/recipe/[recipeId]/page.tsx
@@ -1,13 +1,92 @@
 import React from 'react';
 import s from './page.module.scss';
 import Image from 'next/image';
-import Param from '@/components/recipe-card/Param';
+import axios from 'axios';
 import Category from '@/components/recipe-card/Category';
 import Review from '@/components/review/Review';
 import LeaveReviewForm from '@/components/review/LeaveReviewForm';
 
-const RecipePage = ({ params }: { params: { recipeId: number } }) => {
-	//Req on recipeId
+type Ingredient = {
+	name: string;
+	amount: number;
+	unit: string;
+};
+
+type InstructionStep = {
+	number: number;
+	step: string;
+};
+
+type Recipe = {
+	image: string;
+	title: string;
+	categories: string[];
+	summary: string;
+	readyInMinutes: number;
+	preparationMinutes: number;
+	cookingMinutes: number;
+	ingredients: Ingredient[];
+	instructions: string;
+};
+
+const categoriesKeysToCheck = [
+	'vegetarian',
+	'vegan',
+	'glutenFree',
+	'dairyFree',
+	'veryHealthy',
+	'cheap',
+	'veryPopular',
+	'sustainable',
+	'lowFodmap',
+];
+
+const RecipePage = async ({ params }: { params: { recipeId: number } }) => {
+	const recipeId = params.recipeId;
+
+	const getRecipeById = async (id: number): Promise<Recipe> => {
+		try {
+			const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information`, {
+				params: {
+					apiKey: process.env.SPOONACULAR_API_KEY,
+				},
+			});
+
+			const data = response.data;
+
+			console.log(data);
+			const categories = Object.keys(data).filter(key => data[key] === true);
+
+			const ingredients: Ingredient[] = data.extendedIngredients.map((ingredient: any) => ({
+				name: ingredient.originalName,
+				amount: ingredient.amount,
+				unit: ingredient.unit,
+			}));
+
+			console.log(data.analyzedInstructions[0]?.steps);
+			const instructions: string = data.analyzedInstructions[0]?.steps.map((step: any) => step.step).join(' ') || '';
+
+			const recipe: Recipe = {
+				image: data.image,
+				title: data.title,
+				categories: categoriesKeysToCheck.filter(key => data[key] === true),
+				summary: data.summary,
+				readyInMinutes: data.readyInMinutes,
+				preparationMinutes: data.preparationMinutes || 0,
+				cookingMinutes: data.cookingMinutes || 0,
+				ingredients,
+				instructions,
+			};
+
+			return recipe;
+		} catch (error) {
+			console.error('Error fetching recipe:', error);
+			throw error;
+		}
+	};
+
+	const recipeInfo = await getRecipeById(recipeId);
+	// console.log(recipeInfo);
 
 	function ratingToString(rating: number) {
 		if (rating === 0) return '☆☆☆☆☆';
@@ -16,7 +95,6 @@ const RecipePage = ({ params }: { params: { recipeId: number } }) => {
 		const emptyStars = '☆'.repeat(5 - roundedRating);
 		return filledStars + emptyStars;
 	}
-
 	const stringifyedRating = ratingToString(3.75);
 
 	return (
@@ -24,15 +102,16 @@ const RecipePage = ({ params }: { params: { recipeId: number } }) => {
 			<div className={s.recipePage__container}>
 				<div className={s.recipe}>
 					<div className={s.recipe__img}>
-						<Image src={'/recipe-img.png'} height={350} width={350} alt='recipe-img' />
+						<Image src={recipeInfo.image} height={350} width={350} alt='recipe-img' />
 					</div>
 
 					<div className={s.recipe__mainInfo}>
-						<h3 className={s.title}>Super leek & potato soup</h3>
+						<h3 className={s.title}>{recipeInfo.title}</h3>
 
 						<div className={s.row}>
-							<Category text='Easy' />
-							<Category text='Under 30 mins' />
+							{recipeInfo.categories.map((category, id) => (
+								<Category text={category} key={id} />
+							))}
 						</div>
 
 						<div className={s.horizontalLine}></div>
@@ -45,52 +124,35 @@ const RecipePage = ({ params }: { params: { recipeId: number } }) => {
 						<div className={s.horizontalLine}></div>
 
 						<div className={s.row}>
-							<div className={s.param}>Total time: 1hour</div>
-							<div className={s.param}>Prep time: 40min</div>
-							<div className={s.param}>Cook time: 20min</div>
+							<div className={s.param}>{`Ready in: ${recipeInfo.readyInMinutes}min`}</div>
+
+							{recipeInfo.preparationMinutes > 0 && (
+								<div className={s.param}>{`Prep time: ${recipeInfo.preparationMinutes}min`}</div>
+							)}
+
+							{recipeInfo.cookingMinutes > 0 && (
+								<div className={s.param}>{`Cooking time: ${recipeInfo.cookingMinutes}min`}</div>
+							)}
 						</div>
 
 						<div className={s.horizontalLine}></div>
 
-						<div className={s.description}>
-							I’ve taken family favourite leek and potato soup to the next level here by adding lots of lovely leafy kale,
-							which – along with leeks – gives us vitamin B6, helping us stay awake and alert.
-						</div>
+						<div className={s.description} dangerouslySetInnerHTML={{ __html: recipeInfo.summary }}></div>
 					</div>
 
 					<div className={s.recipe__ingredients}>
 						<h3 className={s.title}>Ingredients</h3>
 						<ul>
-							<li>400g leeks</li>
-							<li>olive oil</li>
-							<li>400 g potatoes</li>
-							<li>1.5 litres organic veg , or chicken stock</li>
-							<li>320g kale</li>
-							<li>½ a bunch of fresh mint , (15g)</li>
-							<li>½ a bunch of fresh flat-leaf parsley , (15g)</li>
-							<li>40 g Parmesan cheese</li>
+							{recipeInfo.ingredients.map((ingredient, id) => (
+								<li key={id}>{ingredient.name}</li>
+							))}
 						</ul>
 					</div>
 
 					<div className={s.recipe__cookProcess}>
 						<h3 className={s.title}>Cooking process</h3>
 
-						<p>
-							Trim and wash 400g of leeks, then slice and place in a large casserole pan on a medium heat with 1 tablespoon of
-							olive oil.
-						</p>
-						<p>
-							Sweat for 10 minutes, stirring regularly, while you wash and finely slice 400g of potatoes (leaving the skin on
-							for extra nutritional benefit), then stir into the pan.
-						</p>
-						<p>
-							Pour in 1.5 litres of vegetable or chicken stock and boil hard for 15 minutes. Pick through 320g of kale,
-							discarding any tough stalks, and add to the pan for the last 5 minutes, pushing it down and covering with a lid.
-						</p>
-						<p>
-							In batches, carefully pour the contents of the pan into a blender. Put the lid on securely, cover with a tea
-							towel and, holding it in place, blitz until super- smooth.
-						</p>
+						<p>{recipeInfo.instructions}</p>
 					</div>
 				</div>
 
